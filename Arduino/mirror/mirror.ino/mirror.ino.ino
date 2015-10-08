ae4384fa7559bbd67f5c9791dc8843dfe676ca0a
nicolsc <nicolas.lesconnec@gmail.com>
2015-10-08 16:04:28 +0200

    Add Arduino - serial mirror sample

---
+++ Arduino/mirror/mirror.ino/mirror.ino.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial sigfox(5,4);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sigfox.begin(9600);
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (sigfox.available()){
+    Serial.write(sigfox.read());
+  }
+  while (Serial.available()){
+    sigfox.write(Serial.read());
+  }
+}
